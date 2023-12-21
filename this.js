e8790ccef31be8ad766bfc61121db727596bc340
bisrabatool <148195865+bisrabatool@users.noreply.github.com>
2023-12-20 21:07:46 -0800

    Add files via upload

---
+++ this.js
@@ -0,0 +1,34 @@
+let obj1 = {
+    name: "harron",
+    displayName: function () {
+        return this.name
+
+    }
+}
+console.log(obj1.displayName());
+function check() {
+
+    console.log(this)
+}
+check();
+let obj = {
+    name: 123,
+    foo: function () {
+        console.log(this.name);
+        setTimeout(function () {
+            console.log(window);
+        }, 2000)
+    }
+}
+obj.foo();
+let obj3 = {
+    name: "bisra",
+    foo1: function () {
+        console.log(this.name);
+        function chk2() {
+            console.log(window);
+        }
+    }
+}
+obj3.foo1();
+
