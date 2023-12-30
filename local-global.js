9010ee19bec816e8033c8d5670cee8b1c123c1c2
Bisra Batool <bisrabatool345@gmail.com>
2023-12-29 22:54:28 -0800

    new

--- local-global.js
+++ local-global.js
@@ -34,35 +34,61 @@
 // console.log(i);
 // 11
 // closure child can access parent's variables
-function bare(){
-    const eidi = "yes"
-    let a = 500
-    var chk = 400
-    function chote(){
-        var chota = 10
-        console.log(eidi);
-        console.log(a);
-    }
-    chote()
-    console.log(chota);
-}
-// bare()
-// console.log(chk);
-if (true) {
-        const username = "faiza"
-        if (username === "faiza") {
-            const employee = " saylani"
-            console.log(username + employee);
-        }
-        // console.log(website);
-    }
-    // its possible
-    // addOne(3) // 4
-    // function addOne(num) {
-    //     return num + 1
+// function bare(){
+//     const eidi = "yes"
+//     let a = 500
+//     var chk = 400
+//     function chote(){
+//         var chota = 10
+//         console.log(eidi);
+//         console.log(a);
+//     }
+//     chote()
+//     console.log(chota);
+// }
+// // bare()
+// // console.log(chk);
+// if (true) {
+//         const username = "faiza"
+//         if (username === "faiza") {
+//             const employee = " saylani"
+//             console.log(username + employee);
+    //     }
+    //     // console.log(website);
     // }
+    // // its possible
+    // // addOne(3) // 4
+    // // function addOne(num) {
+    // //     return num + 1
+    // // }
 
-    addTwo(3) //5
-    const addTwo = function(num) {
-        return num + 2
-    }
+    // addTwo(3) //5
+    // const addTwo = function(num) {
+    //     return num + 2
+    // }
+    
+// globalscope..declare variable out side the function
+var a = "bisra";
+let b = "batool";
+const c = "baig";
+ 
+function check_global_variables(){
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+ 
+check_global_variables();
+// local scope... declare variable inside the function
+function check_function_scope(){
+	var variable1= "moona";
+	let  fatherName= "naeem";
+	const userName= "bushra";
+	console.log(variable1);
+	console.log(fatherName);
+}
+
+check_function_scope();
+console.log(userName);
+
+
